788d6e5dfeeaec15b7112a2cfcbe84a051bbff84
Mario Jr <mariosanchezjr12@gmail.com>
2022-05-04 16:53:35 -0700

    added add, edit, delete modals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -29,5 +30,39 @@ export class AppComponent implements OnInit {
     })
   }
 
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe({
+      next: (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    })
+  }
 
+  public onOpenModal(employee: Employee | null, mode: string): void {
+    const container = document.getElementById('main-container');
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if(mode === 'add'){
+      button.setAttribute('data-target', '#addEmployeeModal')
+    }
+    if(mode === 'edit'){
+      button.setAttribute('data-target', '#editEmployeeModal')
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target', '#deleteEmployeeModal')
+    }
+    container?.appendChild(button);
+    button.click();
+
+  }
 }
